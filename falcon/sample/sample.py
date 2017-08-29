9dbb61db35501fb4cc0ec0379b65f35f02fe2fbd
yui <yuikita21@gmail.com>
2017-08-29 23:18:56 +0900

    reading request

--- falcon/sample/sample.py
+++ falcon/sample/sample.py
@@ -2,10 +2,24 @@ import falcon
 import json
 class Index:
   def on_get(self, req, resp):
+    x = read_req(req)
     default = {
-      'title': 'こんにちは',
-      'numbers': [3, 1, 2]
+      'title': 'helloこんにちは hello',
+      'result': f(x)
       }
     resp.body = json.dumps(default, ensure_ascii=False)
+    resp.status = falcon.HTTP_200
+
+def f(x):
+  x = int(x)
+  return x * 5
+
+def read_req(req, default=0):
+  rst = req.stream.read()
+  if rst != b'':
+    return rst
+  else:
+    return default
+
 api = falcon.API()
 api.add_route('/', Index())
