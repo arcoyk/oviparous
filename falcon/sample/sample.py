1c534ca788b3a5a335a33ba13f3dbafc4718fe16
yui <yuikita21@gmail.com>
2017-08-29 22:24:04 +0900

    format

--- falcon/sample/sample.py
+++ falcon/sample/sample.py
@@ -1,22 +1,11 @@
 import falcon
 import json
-
-class ItemsResource:
-    def on_get(self, req, resp):
-        """Handles GET requests"""
-        items = {
-            'title': 'Python+FalconでWebAPI',
-            'tags': [
-                {
-                    'name': 'Python','versions':[]
-                },
-                {
-                    'name': 'Falcon','vresions':[]
-                }
-            ]
-        }
-
-        resp.body = json.dumps(items,ensure_ascii=False)
-
+class Index:
+  def on_get(self, req, resp):
+    default = {
+      'title': 'こんにちは',
+      'numbers': [3, 1, 2]
+      }
+    resp.body = json.dumps(default, ensure_ascii=False)
 api = falcon.API()
-api.add_route('/items', ItemsResource())
+api.add_route('/', Index())
