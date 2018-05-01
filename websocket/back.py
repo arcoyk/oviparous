8faf37605ba9b42308458217a801cd8cd9607d77
yui <yuikita21@gmail.com>
2018-05-01 17:35:39 +0900

    fix

--- websocket/back.py
+++ websocket/back.py
@@ -9,7 +9,10 @@ async def time(websocket, path):
         await websocket.send(now)
         await asyncio.sleep(random.random() * 3)
 
-start_server = websockets.serve(time, '127.0.0.1', 5678)
+host = '127.0.0.1'
+port = 5678
+start_server = websockets.serve(time, host, port)
+print("Running", host, port)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
